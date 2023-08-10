259c79bce15e4be14b6b4fd7d359527f011f72af
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-10 10:37:53 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function demonstrates the use of printf to print.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        /* Print a message using printf */
+        printf("with proper grammer, but the outcome is a piece of art,");
+        return (0);
+}
