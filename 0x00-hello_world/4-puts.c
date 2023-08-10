1dfe5360131d5089819528e699e921d18cf076a0
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-10 10:16:20 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void)
-/* The Main function to print exactly string */
 {
+/* The Main function to print exactly string */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
