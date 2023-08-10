7dceab7153f35d6831ed84cfb9ba58918f029169
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-10 10:29:15 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
 /**
- *  Main function print string "Programming is like building a multilingual puzzle"
- * int ,return 0.
+ * Simple C file to ptint a String with new line
  */
 
 int main(void)
+/**
+ * Void function uses puts
+ */
 {
 puts("\"Programming is like building a multilingual puzzle");
+/**
+ * Return Value 0
+ */
 return (0);
 }
