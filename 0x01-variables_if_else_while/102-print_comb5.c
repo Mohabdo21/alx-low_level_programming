0c5f70748976fb04e67626954e34083bd1e50599
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-11 12:27:14 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i, j;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+putchar('\n');
+return (0);
+}
