8104d9a544aca507a8bbda78a0ae1af05a4ddedf
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-11 10:35:30 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+putchar('\n');
+return (0);
+}
