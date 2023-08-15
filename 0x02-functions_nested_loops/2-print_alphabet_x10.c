3d4635ef2d4621ddd28c26d397cd0f49514cb86c
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-15 12:48:42 -0700

    2. 10 x alphabet, Betty Style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet ten times followed by new lines
+ * print_alphabet_x10 - Prints the lowercase
+ * alphabet ten times followed by new lines
  */
 void print_alphabet_x10(void)
 {
 	char letter;
 	int i;
 
-	for (i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
     	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
