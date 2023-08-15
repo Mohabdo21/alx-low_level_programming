a599fbbedf1cfa1ed7e9a9e5848bb92a5f25c613
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-15 14:26:22 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,8 +22,7 @@ void times_table(void)
 		if (product < 10)
 		_putchar(' ');
 
-		if (product >= 10)
-		_putchar((product / 10) + '0');
+	_putchar((product / 10) + '0');
 
 	_putchar((product % 10) + '0');
 	}
