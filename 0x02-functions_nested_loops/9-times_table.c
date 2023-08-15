495d396aa894e426f3ab7637aeb99ea7de22cb0a
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-15 14:03:54 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+		product = row * column;
+
+		if (column != 0)
+		_putchar(',');
+
+		else if (column != 0 || product >= 10)
+		_putchar(' ');
+
+
+		if (product < 10 && column != 0)
+		_putchar(' ');
+
+		if (product < 10)
+		_putchar(' ');
+
+		if (product >= 10)
+		_putchar((product / 10) + '0');
+
+	_putchar((product % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void jack_bauer(void);
+void times_table(void);
 
 #endif /* MAIN_H */
