8d9f10c6c44adb6dbdb413773059b2168e44fe51
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-16 02:49:20 -0700

    Adding Functions Diclaration

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,19 @@
 #define MAIN_H
 
 int calculate_multiples_sum(int limit);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_number(int n);
+int _putchar(char c);
+void print_times_table(int n);
+
 
 #endif /* MAIN_H */
