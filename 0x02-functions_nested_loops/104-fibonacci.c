876061d7f17d27c325a8fabf128c7d8c55a560bd
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-15 18:15:39 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,85 @@
 #include <stdio.h>
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+
+#define MAX_DIGITS 1000
+
+typedef struct
 {
-	unsigned int n1 = 1;
-	unsigned int n2 = 2;
-	unsigned int next;	
+	int digits[MAX_DIGITS];
+}
+BigInt;
+
+void initBigInt(BigInt *num)
+{
+	int i;
+	for (i = 0; i < MAX_DIGITS; i++) {
+	num->digits[i] = 0;
+	}
+}
+
+void addBigInt(BigInt *result, const BigInt *a, const BigInt *b)
+{
+	int carry = 0;
+	int i;
+	for (i = MAX_DIGITS - 1; i >= 0; i--) {
+		int sum = a->digits[i] + b->digits[i] + carry;
+		result->digits[i] = sum % 10;
+		carry = sum / 10;
+	}
+}
+
+void copyBigInt(BigInt *dest, const BigInt *src) {
+	int i;
+
+	for (i = 0; i < MAX_DIGITS; i++)
+	{
+	dest->digits[i] = src->digits[i];
+	}
+}
+
+void printBigInt(const BigInt *num)
+{
+	int leadingZeroes = 1;
+	int i;
+
+	for (i = 0; i < MAX_DIGITS; i++) {
+	if (leadingZeroes && num->digits[i] != 0)
+	{
+		leadingZeroes = 0;
+	}
+	if (!leadingZeroes)
+		{
+		printf("%d", num->digits[i]);
+		}
+	}
+}
+
+int main(void) {
+
+/**
+ *
+ *  main - Entry point of the program
+ *
+ *  Return: 0 Always (Success)
+ */
 	int count;
 
-	printf("%d, %d", n1, n2);
+	BigInt n1, n2, next;
+	initBigInt(&n1);
+	initBigInt(&n2);
+	initBigInt(&next);
 
+	n1.digits[MAX_DIGITS - 1] = 1;
+	n2.digits[MAX_DIGITS - 1] = 2;
 
 	for (count = 2; count < 98; count++)
 	{
-	next = n1 + n2;
+	addBigInt(&next, &n1, &n2);
 
-	printf(", %d", next);
+	printf(", ");
+	printBigInt(&next);
 
-
-	n1 = n2;
-	n2 = next;
+	copyBigInt(&n1, &n2);
+	copyBigInt(&n2, &next);
 	}
 
 	printf("\n");
---
+++ 0x02-functions_nested_loops/104-test.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+#define MAX_DIGITS 1000
+
+typedef struct {
+    int digits[MAX_DIGITS];
+} BigInt;
+
+void initBigInt(BigInt *num) {
+    int i;
+    for (i = 0; i < MAX_DIGITS; i++) {
+        num->digits[i] = 0;
+    }
+}
+
+void addBigInt(BigInt *result, const BigInt *a, const BigInt *b) {
+    int carry = 0;
+    int i;
+    for (i = MAX_DIGITS - 1; i >= 0; i--) {
+        int sum = a->digits[i] + b->digits[i] + carry;
+        result->digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+}
+
+void copyBigInt(BigInt *dest, const BigInt *src) {
+    int i;
+    for (i = 0; i < MAX_DIGITS; i++) {
+        dest->digits[i] = src->digits[i];
+    }
+}
+
+void printBigInt(const BigInt *num) {
+    int leadingZeroes = 1;
+    int i;
+    for (i = 0; i < MAX_DIGITS; i++) {
+        if (leadingZeroes && num->digits[i] != 0) {
+            leadingZeroes = 0;
+        }
+        if (!leadingZeroes) {
+            printf("%d", num->digits[i]);
+        }
+    }
+}
+
+int main(void) {
+    int count;  /* Move the declaration to the beginning of the block */
+    BigInt n1, n2, next;
+    initBigInt(&n1);
+    initBigInt(&n2);
+    initBigInt(&next);
+
+    n1.digits[MAX_DIGITS - 1] = 1;
+    n2.digits[MAX_DIGITS - 1] = 2;
+
+    for (count = 2; count < 98; count++) {
+        addBigInt(&next, &n1, &n2);
+
+        printf(", ");
+        printBigInt(&next);
+
+        copyBigInt(&n1, &n2);
+        copyBigInt(&n2, &next);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
