a50ebf4b8f3fe39c02cf1d0daddeef24be3a1a9d
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-16 05:34:10 -0700

    0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,7 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * positive_or_negative - determint which
+ * number is bigger
+ * i - the well checked
+ */
+
 void positive_or_negative(int i)
 {
+
 if (i > 0)
 	printf("%d is positive\n", i);
 	else if (i == 0)
