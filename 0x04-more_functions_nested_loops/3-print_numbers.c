7e35cbc9a27db147dc486f2bcecf838e05ebba15
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-08-17 07:15:51 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - print number from (0) to (9) followed by a new line.
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	_putchar(i);
+	}
+
+	_putchar('\n');
+}
