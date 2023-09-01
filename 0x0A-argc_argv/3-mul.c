0166144d4835f7624312e59d50441e8683e875c2
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-01 09:49:51 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	result = atoi(argv[1]) * atoi (argv[2]);
+	result = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", result);
 
