a2f90a9703e4aa27181498d7144049b1a6619898
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-01 09:10:25 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/*
- * Program that print it's name
- *
- * Return: 0 Always success
+/**
+ * main - Entry point, function to print file name
+ * @argc: count of the arguments supplied
+ * @argv: an array of pointers to the strings
+ *Return: 0 Always success
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	
+
 	printf("%s\n", argv[0]);
 
 	return (0);
