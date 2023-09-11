d88655fa8cadf9ac88f5e5f7c0a89b822e25aa1c
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-11 10:16:02 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Contains a Dog data
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: The name of dog owner
+ */
 struct dog
 {
 	char *name;
