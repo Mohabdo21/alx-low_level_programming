ca8745859c5e980acc3522f036827cd0c663cdb9
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-11 08:16:15 -0700

    0. Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef	0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef	OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
@@ -7,4 +7,4 @@
 
 int _putchar(char c);
 
-#endif /* 0_OBJECT_LIKE_MACRO_H */
+#endif /* OBJECT_LIKE_MACRO_H */
