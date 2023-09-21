6f3205906349662f6880fb2fc547d83ec520b4e5
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-21 09:06:13 -0700

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,21 @@
 section .data
-	msg: db "Hello, Holberton", 10, 0
+    hello db 'Hello, Holberton',10,0
 
 section .text
-
-global main
-extern printf
+    global main
+    extern printf
 
 main:
+    ; Prepare the arguments for printf
+    mov rdi, hello     ; The format, null-terminated string
+    xor rax, rax       ; Because printf is a variadic function,
+                       ; RAX should hold the number of vector registers used.
+                        ; In this case it's 0.
 
-	push rbp
-	mov rbp, rsp
+    ; Call printf
+    call printf
 
-	mov edi, msg
-	mov esi, 0
-
-	call printf
-
-	pop rbp
-
-	ret
+    ; Return 0
+    mov eax, 0x60      ; The syscall number for sys_exit
+    xor edi, edi       ; Exit code 0
+    syscall            ; Call the kernel
