ad9679b367543b36b442cd7edd68dd05001bf59e
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-25 15:36:06 -0700

    Fix 12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,58 @@
 #include "lists.h"
 
 /**
+ * detect_loop - Detects a loop in a linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: Pointer to the start of the loop, or NULL if no loop is detected
+ */
+const listint_t *detect_loop(const listint_t *head)
+{
+	const listint_t *slow_ptr = head;
+	const listint_t *fast_ptr = head;
+
+	while (slow_ptr && fast_ptr && fast_ptr->next)
+	{
+		if (!(fast_ptr->next->next))
+			break;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		if (slow_ptr == fast_ptr)
+		{
+			slow_ptr = slow_ptr->next;
+			return (slow_ptr);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * print_list - Prints a linked list
+ * @head: Pointer to the head of the list
+ * @loop_start: Pointer to the start of a loop in the list, or NULL if no loop
+ *
+ * Return: Number of nodes printed
+ */
+
+size_t print_list(const listint_t *head, const listint_t *loop_start)
+{
+	size_t counter = 0;
+
+	while (head)
+	{
+		counter++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head == loop_start)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			exit(98);
+		}
+		head = head->next;
+	}
+	return (counter);
+}
+
+/**
  * print_listint_safe - Prints a listint_t linked list safely using
  * Floyd's Cycle Detection Algorithm (tortoise and hare approach)
  * @head: A pointer to the head of the list.
@@ -9,30 +61,14 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t node_count = 0;
-	const listint_t *current = head;
-	const listint_t *runner = head;
+	const listint_t *loop_start = detect_loop(head);
 
-	if (head == NULL)
-		exit(98);
-
-	while (current != NULL)
+	if (!loop_start)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		node_count++;
-
-		runner = head;
-		while (runner != current)
-		{
-			if (runner->next == current->next)
-			{
-				printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-				return (node_count);
-			}
-			runner = runner->next;
-		}
-		current = current->next;
+		return (print_list(head, NULL));
 	}
-
-	return (node_count);
+	else
+	{
+		return (print_list(head, loop_start));
+	}
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,5 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+const listint_t *detect_loop(const listint_t *head);
+size_t print_list(const listint_t *head, const listint_t *loop_start);
 
 #endif /* LISTS_H */
