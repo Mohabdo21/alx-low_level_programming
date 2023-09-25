bc58a1fce69c5879183e92305387a8076e552178
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-25 12:36:27 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,42 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely using
- * Floyd's Tortoise and Hare algorithm.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *slow_ptr, *fast_ptr;
+	const listint_t *current = head;
+	const listint_t *loop_start = NULL;
+	size_t node_count = 0;
 
-	if (head == NULL)
-		exit(98);
-
-	slow_ptr = head;
-	fast_ptr = head;
-
-	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
-		nodes++;
-		slow_ptr = slow_ptr->next;
-		fast_ptr  = fast_ptr->next->next;
+		printf("[%p] %d\n", (void *)current, current->n);
+		node_count++;
 
-		if (slow_ptr == fast_ptr)
+	if (current->next >= current)
+	{
+		loop_start = current->next;
+		printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
+		break;
+	}
+
+	current = current->next;
+	}
+
+	if (loop_start != NULL)
+	{
+		current = head;
+		while (current != loop_start)
 		{
-			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
-			return (nodes);
+			printf("[%p] %d\n", (void *)current, current->n);
+			node_count++;
+			current = current->next;
 		}
 	}
 
-	return (nodes);
+	return (node_count);
 }
