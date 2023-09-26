073041fac2ecde2945438b1bd2eb5c0fd5f8a284
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-09-26 16:44:37 -0700

    Fix(12) 12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,7 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 
 		printf("[%p] %d\n", (void *)head, head->n);
 
-		if (variance > 0)
+		if (variance > 0 || (head == head->next))
 			head = head->next;
 		else
 		{
