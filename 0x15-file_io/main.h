870e71811444b58cb3c49aac1e8c988007d883ad
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-10-02 11:24:45 -0700

    1. Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
