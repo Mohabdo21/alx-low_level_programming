d581430767a641fdb745a730cdf4cea8ab52d012
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-13 02:18:07 +0400

    13. Linear search in a skip list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -10,36 +10,42 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *prev, *next;
+	skiplist_t *c_node = list, *p_node = NULL;
 
-	if (!list)
-		return (NULL);
-
-	next = list->express;
-	prev = list;
-
-	while (next && next->n < value)
+	if (!c_node)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", next->index, next->n);
-		prev = next;
-		if (!next->express)
-		{
-			while (next->next)
-				next = next->next;
-			break;
-		}
-		next = next->express;
+		return (NULL);
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n",
-		prev->index, next->index);
-
-	while (prev && prev->n <= next->n)
+	/* Use express lane to get close to the target value */
+	while (c_node->express && c_node->n < value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
+		p_node = c_node;
+		c_node = c_node->express;
+		printf("Value checked at index [%lu] = [%i]\n", c_node->index, c_node->n);
+	}
+
+	/* If we've reached the end of the express lane, move to the next node */
+	if (!c_node->express && c_node->n < value)
+	{
+		p_node = c_node;
+		while (c_node->next)
+		{
+			c_node = c_node->next;
+		}
+	}
+
+	/* Perform linear search in the regular lane */
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		p_node ? p_node->index : 0, c_node->index);
+	while (p_node && p_node->n <= value)
+	{
+		printf("Value checked at index [%lu] = [%i]\n", p_node->index, p_node->n);
+		if (p_node->n == value)
+		{
+			return (p_node);
+		}
+		p_node = p_node->next;
 	}
 
 	return (NULL);
